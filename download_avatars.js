3d9907aa020fc5bbfd8d59fce1c8f49e36b11974
llcoolj84 <llcj84@gmail.com>
2017-09-11 22:17:17 +0000

    successful request

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,48 @@
+var GITHUB_USER = "llcoolj84";
+var GITHUB_TOKEN = "7a67c9ac7c1951d38001ee8c9bf8734567f28005";
 var request = require('request');
+var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  console.log('Welcome to the GitHub Avatar Downloader. We love Tongans!');
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var string = '';
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'moogsG' //User agent for header
+    }
+  };
+  console.log(requestURL);
+  // ...
+
+       request.get(options) // these .get, .on, .on, .on are all "chained" together
+       .on('error', function (err) {
+        throw err;
+       })
+       .on('response', function (response) {
+         response.setEncoding('utf8');
+         console.log('Response Status Message: ', response.statusMessage);
+         console.log('Downloading image...');
+       })
+       .on('data', function (data) {
+       console.log('Chunk Received. Length:', data.length + '\n');
+       string += data;
+       })
+       .on('end', function () {
+       console.log('Download complete.');
+       console.log('Response stream complete.');
+       string = JSON.parse(string);
+       return cb(string);
+       })
+
+
+       //.pipe(fs.createWriteStream('./future.jpg'));
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
