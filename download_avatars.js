8a59bb64fdcccfe8aa265f5544e9d52a6f4e0bbe
llcoolj84 <llcj84@gmail.com>
2017-09-12 01:53:36 +0000

    submit app

--- download_avatars.js
+++ download_avatars.js
@@ -6,51 +6,51 @@ var repoOwner = process.argv[2]; //takes in user input
 var repoName = process.argv[3];
 
 function checkArguments(owner, name) {
-  if (owner === undefined || name === undefined) {
-    console.log("Please enter valid an owner and name!");
-  } else {
-    console.log('Welcome to the GitHub Avatar Downloader!');
-    getRepoContributors(repoOwner, repoName, getJson);
-  }
+    if (owner === undefined || name === undefined) {
+        console.log("Please enter valid owner name and repository!");
+    } else {
+        console.log('Welcome to the GitHub Avatar Downloader!');
+        getRepoContributors(repoOwner, repoName, getJson);
+    }
 }
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  console.log('Welcome to the GitHub Avatar Downloader. We love Tongans!');
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  var string = '';
-  var options = {
-    url: requestURL,
-    headers: {
-      'User-Agent': 'llcoolj84' //User agent for header
-    }
-  };
+    console.log('Welcome to the GitHub Avatar Downloader. We love Tongans!');
+    var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+    var string = '';
+    var options = {
+        url: requestURL,
+        headers: {
+            'User-Agent': 'llcoolj84' //User agent for header
+        }
+    };
 
-  //console.log(requestURL);
-  // ...
+    //console.log(requestURL);
+    // ...
 
-       request.get(options) // these .get, .on, .on, .on are all "chained" together
-       .on('error', function (err) {
-        throw err;
-       })
-       .on('response', function (response) {
-         response.setEncoding('utf8');
-         console.log('Response Status Message: ', response.statusMessage);
-         console.log('Downloading image...');
-       })
-       .on('data', function (data) {
-       string += data;
-       })
-       .on('end', function () {
-       console.log('Download complete.');
-       console.log('Response stream complete.');
-       string = JSON.parse(string);
-       cb(string);
-       })
+    request.get(options) // these .get, .on, .on, .on are all "chained" together
+        .on('error', function(err) {
+            throw err;
+        })
+        .on('response', function(response) {
+            response.setEncoding('utf8');
+            console.log('Response Status Message: ', response.statusMessage);
+            console.log('Downloading image...');
+        })
+        .on('data', function(data) {
+            string += data;
+        })
+        .on('end', function() {
+            console.log('Download complete.');
+            console.log('Response stream complete.');
+            string = JSON.parse(string);
+            cb(string);
+        })
 
 
-       //.pipe(fs.createWriteStream('./future.jpg'));
+    //.pipe(fs.createWriteStream('./future.jpg'));
 }
 
 // getRepoContributors("jquery", "jquery", function(result) {
@@ -60,8 +60,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
 var getJson = function(result) { // get jason key avatar and prints out
 
 
-  for (var j in result)
-  downloadImageByURL(result[j].avatar_url, 'avatar', result[j].login);
+    for (var j in result)
+        downloadImageByURL(result[j].avatar_url, 'avatar', result[j].login);
 
 }
 
@@ -71,21 +71,20 @@ var getJson = function(result) { // get jason key avatar and prints out
 
 function downloadImageByURL(url, filePath, fileName) { //download and write images to "avatar" folder
 
-request.get(url)
-    .on('error', function(err) { //on error
-      console.log(err);
-    })
+    request.get(url)
+        .on('error', function(err) { //on error
+            console.log(err);
+        })
 
- .on('data', function(data) { //on data recieving
-    console.log('Downloading images... ');
-  })
+        .on('data', function(data) { //on data recieving
+            console.log('Downloading images... ');
+        })
 
- .on('end', function() { //on data recieve end
-    console.log("Download complete!");
-  }).pipe(fs.createWriteStream(filePath + "/" + fileName));
+        .on('end', function() { //on data recieve end
+            console.log("Download complete!");
+        }).pipe(fs.createWriteStream(filePath + "/" + fileName));
 
 
 }
 
 checkArguments(repoOwner, repoName);
-
