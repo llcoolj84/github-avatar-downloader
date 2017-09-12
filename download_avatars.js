0281c14a6166da8da98edafe756d7c94338a58e9
llcoolj84 <llcj84@gmail.com>
2017-09-12 01:59:43 +0000

    submit app fixed lint

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,10 @@ var GITHUB_USER = "llcoolj84";
 var GITHUB_TOKEN = "7a67c9ac7c1951d38001ee8c9bf8734567f28005";
 var request = require('request');
 var fs = require('fs');
-var repoOwner = process.argv[2]; //takes in user input
-var repoName = process.argv[3];
+var repoOwner = process.argv[2]; //takes in user input position 2
+var repoName = process.argv[3]; //takes in user input position 3
 
-function checkArguments(owner, name) {
+function checkArguments(owner, name) { // checks for valid arguments
     if (owner === undefined || name === undefined) {
         console.log("Please enter valid owner name and repository!");
     } else {
@@ -15,7 +15,7 @@ function checkArguments(owner, name) {
 }
 
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, cb) { // get repo contributor url's
 
     console.log('Welcome to the GitHub Avatar Downloader. We love Tongans!');
     var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -27,10 +27,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
         }
     };
 
-    //console.log(requestURL);
-    // ...
 
-    request.get(options) // these .get, .on, .on, .on are all "chained" together
+    request.get(options) // request module , these .get, .on, .on, .on are all "chained" together
         .on('error', function(err) {
             throw err;
         })
@@ -48,14 +46,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
             string = JSON.parse(string);
             cb(string);
         })
-
-
-    //.pipe(fs.createWriteStream('./future.jpg'));
 }
-
-// getRepoContributors("jquery", "jquery", function(result) {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
 
 var getJson = function(result) { // get jason key avatar and prints out
 
@@ -67,22 +58,20 @@ var getJson = function(result) { // get jason key avatar and prints out
 
 //getRepoContributors(repoOwner, repoName, getJson); // call get list of url's
 
-
-
 function downloadImageByURL(url, filePath, fileName) { //download and write images to "avatar" folder
 
     request.get(url)
-        .on('error', function(err) { //on error
+        .on('error', function(err) { //message on error
             console.log(err);
         })
 
-        .on('data', function(data) { //on data recieving
+        .on('data', function(data) { //message on data recieving
             console.log('Downloading images... ');
         })
 
-        .on('end', function() { //on data recieve end
+        .on('end', function() { //on message data recieved and end
             console.log("Download complete!");
-        }).pipe(fs.createWriteStream(filePath + "/" + fileName));
+        }).pipe(fs.createWriteStream(filePath + "/" + fileName)); // pipe data from read to write to avatar folder
 
 
 }
